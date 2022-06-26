52dac1a84908c780a7d8b2cd894a39b8310ece3c
Jafar Sidik <jeff.sidiK@gmail.com>
2022-06-26 15:27:32 +0000

    g

---
+++ koperasi_frappe/koperasi_cepjeff/doctype/sektor_usaha/sektor_usaha.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Jafar Sidik and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sektor Usaha', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ koperasi_frappe/koperasi_cepjeff/doctype/sektor_usaha/test_sektor_usaha.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Jafar Sidik and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestSektorUsaha(unittest.TestCase):
+	pass
